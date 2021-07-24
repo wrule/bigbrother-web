da892e67877c14090287630cc7803c1b1a904336
jimao <jimao@jimaodeMacBook-Pro.local>
2021-07-24 15:07:31 +0800

    路由改变

--- app/router.ts
+++ app/router.ts
@@ -4,8 +4,8 @@ export default (app: Application) => {
   const { controller, router } = app;
 
   router.get('/', controller.home.index);
-  router.get('/getAllProjectInfo', controller.home.getAllProjectInfo);
-  router.get('/getProjectApiList', controller.home.getProjectApiList);
-  router.get('/getApiHistory', controller.home.getApiHistory);
-  router.get('/getApiHistoryDetail', controller.home.getApiHistoryDetail);
+  router.get('/api/api/getAllProjectInfo', controller.home.getAllProjectInfo);
+  router.get('/api/api/getProjectApiList', controller.home.getProjectApiList);
+  router.get('/api/api/getApiHistory', controller.home.getApiHistory);
+  router.get('/api/api/getApiHistoryDetail', controller.home.getApiHistoryDetail);
 };
